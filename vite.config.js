c18fb6355141ba91e12e5e618a26cf6a87617a8c
Brandon Quintero <developerbvth@gmail.com>
2023-07-22 00:42:35 -0500

    test Action

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      output: {
-        // Especifica manualmente cómo dividir los módulos en fragmentos
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            // Agrupa los módulos de node_modules en un solo fragmento
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
+  base: "/portafolio_react/",
 });
